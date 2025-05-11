0e498d9faf097c708a77ec83bddd262e1006d570
Nath1710 <nathalyhernandez815@gmail.com>
2025-05-10 20:51:23 -0500

    Nodejs & favicon fixed

--- webpack.common.js
+++ webpack.common.js
@@ -41,7 +41,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      favicon: '4geeks.ico',
+      favicon: 'Datacloth.ico',
       template: 'template.html'
     }),
     new Dotenv({ safe: true, systemvars: true })
